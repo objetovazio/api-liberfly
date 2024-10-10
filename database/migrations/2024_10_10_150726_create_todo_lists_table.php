5f980548fdba48f3077d24baf7b7c1d040b1ebd3
André Martins <objetovazio@gmail.com>
2024-10-10 20:27:21 -0300

    Update 2024_10_10_150726_create_todo_lists_table.php

--- database/migrations/2024_10_10_150726_create_todo_lists_table.php
+++ database/migrations/2024_10_10_150726_create_todo_lists_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('todo_lists', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
+            $table->unique(['name', 'user_id']);
         });
     }
 
